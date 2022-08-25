77c116eb96361d3be0c7bb232bfe03e854993607
ekubay <axuTec14@gmail.com>
2022-08-25 10:57:03 +0300

    building the app streamlit

--- app.py
+++ app.py
@@ -2,35 +2,49 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
-sys.path.insert(0, './pages')
+#sys.path.insert(0, './pages')
 #import numpy as np
-#import pandas as pd
+import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu 
 # the pages
 import pages.main as main1
-import pages.user_overview_analysis as overview
-import pages.user_engagement_analysis as enngagement
-import pages.user_expriance_analysis as experience
-import pages.user_satisfuction_analysis as satisfaction
+from pages.user_overview_analysis import OverviewAnalysis
+#from pages.user_engagement_analysis import engagementAnalysis
+#from pages.user_expriance_analysis import exprianceAnalysis
+#from pages.user_satisfuction_analysis import satisfactionAnalysis
+
+# reading the file 
+df = pd.read_csv('data/clean_df_tel1.csv')
 
 st.set_page_config(page_title="Telecom Data Analysis", layout="wide")
-st.title("TelCo Data analysis")
+st.title("Telcom Data analysis")
 st.sidebar.markdown("# TellCo Data Analysis")
 #page = st.sidebar.selectbox('TellCo Menu', ['Intro', 'Marketing', 'Engagement', 'Experiance', 'Satisfaction'])
 with st.sidebar:
-  page = option_menu('TellCo Menu', ['Main', 'overview analysis', 'Engagement', 'Experience', 'Satisfaction'],
-                            icons=['house', 'bi-currency-exchange','bi-cloud-check-fill', 'bi-briefcase-fill','bi-check-square-fill'], menu_icon="cast", default_index=1)
+  #'Engagement', 'Experience', 'Satisfaction'
+   #'bi-cloud-check-fill', 'bi-briefcase-fill','bi-check-square-fill'], menu_icon="cast", 
+  page = option_menu('Menu', ['Main', 'Overview'],
+                            icons=['house', 'bi-currency-exchange'],
+                            
+                           default_index=1)
   page
-if(page == 'Home'):
+  
+  overview = OverviewAnalysis(df)
+  #engagement = engagementAnalysis(df)
+  #expriance = exprianceAnalysis(df)
+  #satisfaction = satisfactionAnalysis(df)
+
+
+if(page == 'Main'):
   main1.run()
-elif(page == 'Overview')
-  marketing.run_verview()
-elif(page == 'Engagement'):
-  enngagement.run_engagement()
-elif(page == 'Experience'):
-  experience.run_experiance()
-elif(page == 'Satisfaction'):
-  satisfaction.run_satisfaction()
+elif(page == 'Overview'):
+  overview.run_overview()
+# elif(page == 'Engagement'):
+#   enngagement.run_engagement()
+# elif(page == 'Experience'):
+#   experience.run_experiance()
+# elif(page == 'Satisfaction'):
+#   satisfaction.run_satisfaction()
 else:
   main1.run()
