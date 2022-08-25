fa32294fd40f2a4f6c653588737ef1ea40ce2a7b
ekubay <axuTec14@gmail.com>
2022-08-25 22:00:32 +0300

    updating app

--- app.py
+++ app.py
@@ -2,11 +2,14 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
-#sys.path.insert(0, './pages')
+sys.path.insert(0, './pages')
+sys.path.insert(0, './tests')
 #import numpy as np
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu 
+st.set_page_config(page_title='Telecommunication Data Analysis', page_icon=None, layout="centered", 
+initial_sidebar_state="auto", menu_items=None)
 # the pages
 import pages.main as main1
 from pages.user_overview_analysis import OverviewAnalysis
@@ -14,10 +17,7 @@ from pages.user_overview_analysis import OverviewAnalysis
 #from pages.user_expriance_analysis import exprianceAnalysis
 #from pages.user_satisfuction_analysis import satisfactionAnalysis
 
-# reading the file 
-df = pd.read_csv('data/clean_df_tel1.csv')
-
-st.set_page_config(page_title="Telecom Data Analysis", layout="wide")
+#st.set_page_config(page_title="Telecom Data Analysis", layout="wide")
 st.title("Telcom Data analysis")
 st.sidebar.markdown("# TellCo Data Analysis")
 #page = st.sidebar.selectbox('TellCo Menu', ['Intro', 'Marketing', 'Engagement', 'Experiance', 'Satisfaction'])
@@ -30,21 +30,21 @@ with st.sidebar:
                            default_index=1)
   page
   
+  df = pd.read_csv('clean_df_tel1.csv')
   overview = OverviewAnalysis(df)
   #engagement = engagementAnalysis(df)
   #expriance = exprianceAnalysis(df)
   #satisfaction = satisfactionAnalysis(df)
-
-
-if(page == 'Main'):
-  main1.run()
-elif(page == 'Overview'):
-  overview.run_overview()
+  #df = pd.read_csv('data/clean_df_tel1.csv')
+  if(page == 'Main'):
+   main1.run()
+  elif(page == 'Overview'):
+   overview.run_overview()
 # elif(page == 'Engagement'):
 #   enngagement.run_engagement()
 # elif(page == 'Experience'):
 #   experience.run_experiance()
 # elif(page == 'Satisfaction'):
 #   satisfaction.run_satisfaction()
-else:
-  main1.run()
+  else:
+    main1.run()
