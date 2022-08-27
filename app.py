3c585c702904be51c5507d564fba29b2877d3bb7
ekubay <axuTec14@gmail.com>
2022-08-27 10:20:12 +0300

    building app

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ from streamlit_option_menu import option_menu
 st.set_page_config(page_title='Telecommunication Data Analysis1', page_icon=None, layout="centered", 
 initial_sidebar_state="auto", menu_items=None)
 # the pages
-import pages.main as main1
-from pages.user_overview_analysis import OverviewAnalysis
-#from pages.user_engagement_analysis import engagementAnalysis
-#from pages.user_expriance_analysis import exprianceAnalysis
-#from pages.user_satisfuction_analysis import satisfactionAnalysis
+import dashboard.main as main1
+from dashboard.user_overview_analysis import OverviewAnalysis
+import dashboard.user_engagement_analysis as engage
+import dashboard.user_expriance_analysis as expriance
+import dashboard.user_satisfuction_analysis as satisfy
 
 #st.title("Telecomunication Data analysis2")
 #st.sidebar.markdown("# Tellcomunication Data Analysis")
@@ -23,27 +23,29 @@ from pages.user_overview_analysis import OverviewAnalysis
 with st.sidebar:
   #'Engagement', 'Experience', 'Satisfaction'
    #'bi-cloud-check-fill', 'bi-briefcase-fill','bi-check-square-fill'], menu_icon="cast", 
-  page = option_menu('Menu', ['Main', 'Overview'],
-                            icons=['house', 'bi-currency-exchange'],
-                            
-                           default_index=1)
+  page = option_menu('Menu', ['Main', 'Overview','Engagement', 'Experience', 'Satisfaction'],
+                            icons=['house', 'bi-currency-exchange','bi-cloud-check-fill', 'bi-briefcase-fill',
+                            'bi-check-square-fill'], menu_icon="cast", default_index=1)
   page
   
+  
   df = pd.read_csv('clean_df_tel1.csv')
+  #df1 = pd.read_csv('tele-data.csv')
+
   overview = OverviewAnalysis(df)
   #engagement = engagementAnalysis(df)
-  #expriance = exprianceAnalysis(df)
+  #expriance = exprianceAnalysis(df1)
   #satisfaction = satisfactionAnalysis(df)
   #df = pd.read_csv('data/clean_df_tel1.csv')
   if(page == 'Main'):
    main1.run()
   elif(page == 'Overview'):
    overview.run_overview()
-# elif(page == 'Engagement'):
-#   enngagement.run_engagement()
-# elif(page == 'Experience'):
-#   experience.run_experiance()
-# elif(page == 'Satisfaction'):
-#   satisfaction.run_satisfaction()
+  elif(page == 'Engagement'):
+    engage.run_engagement()
+  elif(page == 'Experience'):
+    expriance.run_experiance()
+  elif(page == 'Satisfaction'):
+    satisfy.run_satisfaction()
   else:
     main1.run()
