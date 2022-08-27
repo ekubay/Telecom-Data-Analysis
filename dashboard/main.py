59bca095347b6614297d983aab5e2b0205b0ac8a
ekubay <axuTec14@gmail.com>
2022-08-27 15:25:02 +0300

    adding main

---
+++ dashboard/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def run():
+  st.write("Tellecomminication data analysis")
+  st.write("This project is performed to analyse whether Tellco is worth buying and selling.\
+           To perform the project the data extracted from a month of aggregated data on xDR is used.\
+           In this work, data cleaning, transforming, exploration and analysis tasks are performed.\
+           Finally,user overview, engagement, experience and satisfaction analytics is done and the Telcco productivity is predicted")
+  
